1db5cbe714fdaee9ef3f1aefa5dac17e958730d9
Jerry Ren <renjwei@yahoo.com.cn>
2012-09-10 17:59:55 +0800

    add item and page class to store parsed xml data

--- com.fonenet.fonemarket.MainActivity/src/com/fonenet/fonemarket/FoneNetXmlParser.java
+++ com.fonenet.fonemarket.MainActivity/src/com/fonenet/fonemarket/FoneNetXmlParser.java
@@ -15,76 +15,103 @@ public class FoneNetXmlParser {
 	// We don't use namespaces
 	private static final String ns = null;
 
-	public class Person {
-		private Integer id;
-		private String name;
-		private Short age;
-
-		public Integer getId() {
-			return id;
+	public class Page {
+		String name;
+		Integer itemNum;
+		ArrayList<Item> items;
+		Page(){
+			name = null;
+			itemNum = 0;
+			items = new ArrayList<Item>();
 		}
-
-		public void setId(Integer id) {
-			this.id = id;
+		public void setName(String setName) {
+			name = setName;
 		}
-
 		public String getName() {
 			return name;
 		}
-
-		public void setName(String name) {
-			this.name = name;
+		public Integer getItemNum(){
+			return itemNum;
 		}
-
-		public Short getAge() {
-			return age;
+		public Integer addItem(Item item){
+			itemNum++;
+			items.add(item);
+			return itemNum;
 		}
+	};
+	public class Item {
+		String attr;
+		String binName;
+		Integer appSize;
+		String fee; //no use
+		String appId;
+		String iconName;
+		String intro;
+		String name; //item name
+		String start; //no use
+		String time;
+		String type;
+		String version;
+	};
 
-		public void setAge(Short age) {
-			this.age = age;
-		}
-	}
+	Integer pageNum;
+	ArrayList<Page> pages;
 
 	@SuppressLint("UseValueOf")
-	public List<Person> readXML(InputStream inStream) {
+	public ArrayList<Page> readXML(InputStream inStream) {
 		XmlPullParser parser = Xml.newPullParser();
 		try {
 			parser.setInput(inStream, "UTF-8");
 			int eventType = parser.getEventType();
-			Person currentPerson = null;
-			List<Person> persons = null;
+			Page currentPage = null;
+			Item curItem = null;
 			while (eventType != XmlPullParser.END_DOCUMENT) {
 				switch (eventType) {
 				case XmlPullParser.START_DOCUMENT:// 文档开始事件,可以进行数据初始化处理
-					persons = new ArrayList<Person>();
+					pages = new ArrayList<Page>();
+					pageNum = 0;
 					break;
 				case XmlPullParser.START_TAG:// 开始元素事件
 					String name = parser.getName();
-					if (name.equalsIgnoreCase("person")) {
-						currentPerson = new Person();
-						currentPerson.setId(new Integer(parser
-								.getAttributeValue(null, "id")));
-					} else if (currentPerson != null) {
-						if (name.equalsIgnoreCase("name")) {
-							currentPerson.setName(parser.nextText());// 如果后面是Text元素,即返回它的值
-						} else if (name.equalsIgnoreCase("age")) {
-							currentPerson.setAge(new Short(parser.nextText()));
+					if (name.equalsIgnoreCase("page")) {
+						currentPage = new Page();
+						currentPage.setName(new String(parser.getAttributeValue(null, "name")));
+					} else if (currentPage != null) {
+						if (name.equalsIgnoreCase("item")) {
+							curItem = new Item();
+							curItem.attr = new String(parser.getAttributeValue(null, "attr"));
+							curItem.binName = new String(parser.getAttributeValue(null, "bin"));
+							curItem.appSize = new Integer(parser.getAttributeValue(null, "binsize"));
+							curItem.fee = new String(parser.getAttributeValue(null, "fee"));
+							curItem.appId = new String(parser.getAttributeValue(null, "id"));
+							curItem.iconName = new String(parser.getAttributeValue(null, "image"));
+							curItem.intro = new String(parser.getAttributeValue(null, "intro"));
+							curItem.name = new String(parser.getAttributeValue(null, "name"));
+							curItem.start = new String(parser.getAttributeValue(null, "start"));
+							curItem.time = new String(parser.getAttributeValue(null, "time"));
+							curItem.type = new String(parser.getAttributeValue(null, "type"));
+							curItem.version = new String(parser.getAttributeValue(null, "version"));
 						}
 					}
 					break;
 
 				case XmlPullParser.END_TAG:// 结束元素事件
-					if (parser.getName().equalsIgnoreCase("person")
-							&& currentPerson != null) {
-						persons.add(currentPerson);
-						currentPerson = null;
+					if (parser.getName().equalsIgnoreCase("page")
+							&& currentPage != null) {
+						pages.add(currentPage);
+						currentPage = null;
+					}
+					else if (parser.getName().equalsIgnoreCase("item")
+							&& currentPage != null) {
+						currentPage.addItem(curItem);
+						curItem = null;
 					}
 					break;
 				}
 				eventType = parser.next();
 			}
 			inStream.close();
-			return persons;
+			return pages;
 
 		} catch (Exception e) {
 			e.printStackTrace();
